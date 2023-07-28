17f5b19752e2c9795d33c2d7b44e2802ec7afe54
mohamed-elghafiani <medghafiani@gmail.com>
2023-07-27 17:08:07 -0700

    Implimented selection sort algorithm to sort an array of integers

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "sort.h"
 
 
@@ -11,4 +12,24 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j, min_index, tmp;
+
+	for (i = 0; i < size; i++)
+	{
+		min_index = i;
+		for (j = i; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+			{
+				min_index = j;
+			}
+		}
+		if (array[i] != array[min_index])
+		{
+			tmp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = tmp;
+			print_array(array, size);
+		}
+	}
 }
