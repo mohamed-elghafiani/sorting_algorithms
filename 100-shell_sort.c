023c5f7c35e1bc0146b1c4b5d92e116a621d1cc6
mohamed-elghafiani <medghafiani@gmail.com>
2023-09-30 00:54:58 +0000

    shell sort implimentation

---
+++ 100-shell_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * shell_sort - Sorts an array of integers in ascending order using Shell sort
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, i, j;
+	int temp;
+
+	if (!array || size < 2)
+		return;
+
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	for (; gap > 0; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = temp;
+		}
+		print_array(array, size);
+	}
+}
+
--- sort.h
+++ sort.h
@@ -28,6 +28,6 @@ void selection_sort(int *array, size_t size);
 
 void quick_sort(int *array, size_t size);
 
-
+void shell_sort(int *array, size_t size);
 
 #endif
